53e19bb1391a6b28c3acdba2834897711e3b8d31
VinLvy <davinputrafibrian24@gmail.com>
2024-11-04 17:24:19 +0700

    npm install

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+};
